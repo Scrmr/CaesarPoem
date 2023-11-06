a4e9269eae6ef908d7fccf92cf6e6a9fcae0fbaf
Scrmr <i.zemleckas@gmail.com>
2023-11-06 17:29:11 +0000

    edit1 06.11.2023

--- public/script.js
+++ public/script.js
@@ -1,93 +1,91 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Get references to the DOM elements
-    const wordInput = document.getElementById('word-input');
-    const generateBtn = document.getElementById('generate-btn');
-    const poemContainer = document.getElementById('poem-container');
+  const wordInput = document.getElementById('word-input');
+  const generateBtn = document.getElementById('generate-btn');
+  const poemContainer = document.getElementById('poem-container');
 
-    // Function to validate the input is 10 words or fewer
-    function validateInput(input) {
-        const words = input.split(/\s+/); // Split by any whitespace
-        return words.length <= 10 && words.length > 0;
-    }
+  function validateInput(input) {
+      const words = input.split(/\s+/);
+      return words.length <= 10 && words.length > 0;
+  }
 
-    // Mock function to "encrypt" a word - for now, we'll just reverse it
-    function encryptWord(word) {
-        return word.split('').reverse().join('');
-    }
-
-    // Function to "decrypt" the encrypted word - for now, it's just reversing it back
-    function decryptWord(encryptedWord) {
-        return encryptedWord.split('').reverse().join('');
-    }
-
-    // Function to handle the decryption attempt
-    function handleDecryptionAttempt(encryptedSpan) {
-        const originalWord = decryptWord(encryptedSpan.textContent.trim());
-        const userGuess = prompt('What is the decrypted word?').trim();
-
-        if (userGuess.toLowerCase() === originalWord.toLowerCase()) {
-            encryptedSpan.textContent = originalWord;
-            encryptedSpan.classList.remove('encrypted');
-            encryptedSpan.classList.add('correct'); // Add this new class to highlight correct guesses
-            alert('Correct!');
-        } else {
-            alert('Not quite, try again.');
-        }
-    }
-
-   // Event listener for the generate button
-generateBtn.addEventListener('click', async () => {
-    const userInput = wordInput.value.trim();
-    if (!validateInput(userInput)) {
-        alert('Please enter up to 10 words.');
-        return;
-    }
-
-    try {
-        const response = await fetch('/generate-poem', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ input: userInput }),
-        });
-        const data = await response.json();
-        if (data.poem) {
-            displayPoem(data.poem); // Implement this function to display the poem
-        } else {
-            alert('Failed to generate poem.');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        alert('An error occurred while generating the poem.');
-    }
-});
-
-function displayPoem(poem) {
-    poemContainer.innerHTML = ''; // Clear the previous poem
-    const lines = poem.split('\n'); // Assuming the poem is separated by new lines
-    lines.forEach(line => {
-        const p = document.createElement('p');
-        p.textContent = line;
-        poemContainer.appendChild(p);
-    });
-}
-
-
-function caesarCipher(str, shift) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    return str
-      .toLowerCase()
-      .split('')
-      .map(char => {
-        if (alphabet.includes(char)) {
-          let newIndex = (alphabet.indexOf(char) + shift) % 26;
+  function decryptCaesarCipher(encryptedWord, shift) {
+      const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+      return encryptedWord
+        .split('')
+        .map(char => {
+          const index = alphabet.indexOf(char);
+          if (index === -1) {
+            return char;
+          }
+          const newIndex = (index < alphabet.length / 2)
+            ? (index - shift + alphabet.length / 2) % (alphabet.length / 2)
+            : (alphabet.length / 2 + index - shift) % (alphabet.length / 2) + alphabet.length / 2;
           return alphabet[newIndex];
-        }
-        return char;
-      })
-      .join('');
+        })
+        .join('');
+  }
+
+  function handleDecryptionAttempt(encryptedSpan, shift) {
+      const encryptedWord = encryptedSpan.textContent.trim();
+      const originalWord = decryptCaesarCipher(encryptedWord, shift);
+      const userGuess = prompt('What is the decrypted word?').trim();
+
+      if (userGuess.toLowerCase() === originalWord.toLowerCase()) {
+          encryptedSpan.textContent = originalWord;
+          encryptedSpan.classList.remove('encrypted');
+          alert('Correct!');
+      } else {
+          alert('Not quite, try again.');
+      }
+  }
+
+  generateBtn.addEventListener('click', async () => {
+      const userInput = wordInput.value.trim();
+      if (!validateInput(userInput)) {
+          alert('Please enter up to 10 words.');
+          return;
+      }
+
+      try {
+          const response = await fetch('/generate-poem', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ input: userInput }),
+          });
+          const data = await response.json();
+          if (data.poem && data.encryptedWordIndices) { // Check if encryptedWordIndices is present
+            displayPoem(data.poem, data.encryptedWordIndices);
+           } else {
+            alert('Failed to generate poem.');
+           }
+      } catch (error) {
+          console.error('Error:', error);
+          alert('An error occurred while generating the poem.');
+      }
+  });
+
+  function displayPoem(poem, encryptedWordIndices) {
+    poemContainer.innerHTML = ''; // Clear the previous poem
+    const words = poem.match(/[\w'’]+|[.,!?;"]/g) || []; // Match words and punctuation
+    words.forEach((word, index) => {
+      const span = document.createElement('span');
+      span.textContent = word + ' ';
+      if (encryptedWordIndices.includes(index)) { // Check if the word index is in the encrypted indices
+        span.classList.add('encrypted');
+        span.onclick = function() {
+          handleDecryptionAttempt(span, 3); // Use the correct shift value for decryption
+        };
+      }
+      poemContainer.appendChild(span); // Add the word to the poem container
+  
+      // If the word is a punctuation that typically follows a line break, add a line break
+      if (word.match(/[.!?]/) && index !== words.length - 1) {
+        poemContainer.appendChild(document.createElement('br'));
+      }
+    });
   }
   
-
-    });
+  
+});
--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 require('dotenv').config();
 
 const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+
+// Caesar cipher function
+function caesarCipher(str, shift) {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  return str.split('').map(char => {
+    let index = alphabet.indexOf(char);
+    if(index === -1) {
+      // Character not in the alphabet, leave it as is
+      return char;
+    }
+    let newIndex = (alphabet.length / 2 + index + shift) % (alphabet.length / 2);
+    return alphabet[index < alphabet.length / 2 ? newIndex : newIndex + alphabet.length / 2];
+  }).join('');
+}
+
+// Function to apply Caesar cipher to every 10th word and track encrypted words
+function applyCaesarCipherToPoem(poem, shift) {
+  let words = poem.match(/[\w'’]+|[.,!?;"]/g) || [];
+  let encryptedWordIndices = []; // This will hold the indices of encrypted words
+  for (let i = 9; i < words.length; i += 10) {
+    words[i] = caesarCipher(words[i], shift);
+    encryptedWordIndices.push(i); // Store the index of the encrypted word
+  }
+  return { cipheredPoem: words.join(' '), encryptedWordIndices };
+}
+
 
 async function generatePoemWithAI(input) {
   try {
     const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
       prompt: input,
-      max_tokens: 150, // Adjust based on how long you want the poem to be
-      temperature: 0.7, // Adjust for creativity
+      max_tokens: 150,
+      temperature: 0.7,
     }, {
       headers: {
         'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
@@ -17,26 +45,21 @@ async function generatePoemWithAI(input) {
     return response.data.choices[0].text;
   } catch (error) {
     console.error('Error calling OpenAI API:', error);
-    return null; // Handle the error appropriately in your application
+    return null;
   }
 }
 
-
-
-
-const express = require('express');
 const app = express();
-const cors = require('cors');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 
-
 app.post('/generate-poem', async (req, res) => {
   const userInput = req.body.input;
-  const poem = await generatePoemWithAI(userInput);
-  if (poem) {
-    res.json({ poem });
+  const rawPoem = await generatePoemWithAI(userInput);
+  if (rawPoem) {
+    const { cipheredPoem, encryptedWordIndices } = applyCaesarCipherToPoem(rawPoem, 3); // Apply the cipher to the poem
+    res.json({ poem: cipheredPoem, encryptedWordIndices });
   } else {
     res.status(500).json({ error: 'Could not generate poem' });
   }
@@ -46,14 +69,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// Add this function within your server.js
-function applyCaesarCipherToPoem(poem, shift) {
-  return poem.split(' ').map(word => {
-    // Randomly decide to encrypt the word or not
-    if (Math.random() > 0.5) {
-      return caesarCipher(word, shift);
-    }
-    return word;
-  }).join(' ');
-}
