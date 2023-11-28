d3239953a3374ad43bec420deb4aec4734307d13
Scrmr <i.zemleckas@gmail.com>
2023-11-28 14:25:41 +0000

    28.11.2023

--- q_learning_backend.py
+++ q_learning_backend.py
@@ -1,8 +1,21 @@
-import sqlite3
+import mysql.connector
+import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+
 
 # Global counter for updates and threshold for saving
 update_counter = 0
@@ -17,40 +30,56 @@ class UpdateRequest(BaseModel):
     reward: int
     next_state: str
 
+
+
 def get_db_connection():
-    conn = sqlite3.connect('qlearning_game.db')
-    conn.row_factory = sqlite3.Row
+    conn = mysql.connector.connect(
+        host=os.getenv("host", "35.197.250.32"),
+        user=os.getenv("user", "probability2"),
+        password=os.getenv("password", "KilimasMieg4.."),
+        database=os.getenv("database", "probability2")
+    )
     return conn
 
-def initialize_q_table():
+
+def initialize_db():
     conn = get_db_connection()
-    conn.execute('''
+    cursor = conn.cursor()
+    cursor.execute('''
         CREATE TABLE IF NOT EXISTS q_table (
-            state TEXT PRIMARY KEY,
-            action_A REAL,
-            action_D REAL
-        )''')
-    conn.execute("INSERT OR IGNORE INTO q_table (state, action_A, action_D) VALUES ('A', 0, 0), ('D', 0, 0)")
+            state VARCHAR(50) PRIMARY KEY,
+            action_A FLOAT,
+            action_D FLOAT
+        )
+    ''')
+    cursor.execute("INSERT IGNORE INTO q_table (state, action_A, action_D) VALUES ('A', 0, 0), ('D', 0, 0)")
     conn.commit()
     conn.close()
 
+
 def get_q_values(state):
     conn = get_db_connection()
-    q_values = conn.execute('SELECT * FROM q_table WHERE state = ?', (state,)).fetchone()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM q_table WHERE state = %s', (state,))
+    q_values = cursor.fetchone()
     conn.close()
     return q_values if q_values else {'action_A': 0, 'action_D': 0}
 
 def update_q_table(state, action, reward, next_state, alpha, gamma):
     global update_counter
     conn = get_db_connection()
+    cursor = conn.cursor()
+
     current_q = get_q_values(state)[f'action_{action}']
     max_next_q = max(get_q_values(next_state).values())
     new_q = current_q + alpha * (reward + gamma * max_next_q - current_q)
-    conn.execute(f'UPDATE q_table SET action_{action} = ? WHERE state = ?', (new_q, state))
+
+    cursor.execute(f'UPDATE q_table SET action_{action} = %s WHERE state = %s', (new_q, state))
     conn.commit()
     conn.close()
 
     update_counter += 1
+
 
 @app.post("/predict")
 def predict(request_data: PredictionRequest):
@@ -70,6 +99,7 @@ def update(request_data: UpdateRequest):
 
 
 if __name__ == "__main__":
-    initialize_q_table()
+    initialize_db()
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
