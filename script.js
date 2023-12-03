b2c9bf1928e35594e9cb1822f8918dab765b3aab
Scrmr <i.zemleckas@gmail.com>
2023-12-03 23:10:19 +0000

    03.11.2023 edit1

--- script.js
+++ script.js
@@ -92,3 +92,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
   
   
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var acc = document.getElementsByClassName('accordion-header');
+  for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function() {
+      this.classList.toggle('active');
+      var content = this.nextElementSibling;
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
+    });
+  }
+});
+
